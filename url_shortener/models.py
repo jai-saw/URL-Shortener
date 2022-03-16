f7cec7e48e983cb5329381ef5ca3a577e076d719
Jai Sawhney <jai.sawhney1@gmail.com>
2022-03-16 14:54:56 -0700

    Update models

--- url_shortener/models.py
+++ url_shortener/models.py
@@ -5,10 +5,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
+    user = db.relationship("Url", backref="user", cascade="delete,all")
 
 
 class Url(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     original_url = db.Column(db.String, nullable=False)
     shortened_id = db.Column(db.String, nullable=False)
     created_on = db.Column(db.Date, nullable=False)
